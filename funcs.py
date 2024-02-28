5d0062e0c11a00c93a1a5e5c4ec75cc30bffcb8f
Iv-EN <engenivanov@yandex.ru>
2024-02-28 21:55:54 +0300

    Added import of get_sums to main.py

--- funcs.py
+++ funcs.py
@@ -1,8 +1,8 @@
 def get_summ(a: float, b: float) -> str:
     """Складывает два числа."""
-    return f'a + b = {a + b}'
+    return f'{a} + {b} = {a + b}'
 
 
 def get_number_difference(a: float, b: float) -> str:
     """Получает разность двух чисел."""
-    return f'a - b = {a - b}'
+    return f'{a} - {b} = {a - b}'
